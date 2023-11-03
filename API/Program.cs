4bc5f0cfc0f39b4e7cdbe15cd24a58cfbc7e9950
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-11-03 16:59:37 -0500

    feat: :sparkles: Cambios en el archivo Program, incluye el Serilog, la lectura y carga de datos definidos, inclusion de las extensiones y demas detalles

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,27 @@
+using System.Reflection;
+using API.Extensions;
+using AspNetCoreRateLimit;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var logger = new LoggerConfiguration()
+					.ReadFrom.Configuration(builder.Configuration)
+					.Enrich.FromLogContext()
+					.CreateLogger();
+
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureRatelimiting();
+builder.Services.ConfigureApiVersioning();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+builder.Services.AddAplicacionServices();
+builder.Services.ConfigureCors();
 
 builder.Services.AddDbContext<DbFirstContext>(options =>
 {
@@ -21,7 +37,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-using (var scope = app.Services.CreateScope())
+// * Constructor de DbFirst solamente
+/* using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var loggerFactory = services.GetRequiredService<ILoggerFactory>();
@@ -35,9 +52,33 @@ using (var scope = app.Services.CreateScope())
         var _logger = loggerFactory.CreateLogger<Program>();
         _logger.LogError(ex, "Ocurrio un error durante la migracion!");
     }
+} */
+
+// * Constructor de DbFirst y Carga de Datos Predefinidos
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+	var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+	try
+	{
+		var context = services.GetRequiredService<DbFirstContext>();
+		await context.Database.MigrateAsync();
+		await DbFirstContextSeed.SeedAsync(context,loggerFactory);
+	}
+	catch (Exception ex)
+	{
+		var _logger = loggerFactory.CreateLogger<Program>();
+		_logger.LogError(ex, "Ocurrio un error durante la migracion!");
+	}
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
+
+app.UseIpRateLimiting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
