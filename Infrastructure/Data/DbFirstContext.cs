e6f405dccc2e4d7047a0a0a7f64cf8b984e46eb3
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-11-03 15:58:42 -0500

    feat: :card_file_box: Archivo DbFirstContext, se eliminaron las respectivas configuraciones

---
+++ Infrastructure/Data/DbFirstContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace Infrastructure.Data;
+
+public partial class DbFirstContext : DbContext
+{
+    public DbFirstContext()
+    {
+    }
+
+    public DbFirstContext(DbContextOptions<DbFirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Driver> Drivers { get; set; }
+    public virtual DbSet<Team> Teams { get; set; }
+    public DbSet<TeamDriver> TeamDrivers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySql("server=localhost;user=root;password=1122809631;database=formularace", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
