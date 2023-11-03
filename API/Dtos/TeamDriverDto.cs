b1ce2c4cd848735e26df50720ba23390753b37e3
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-11-03 16:58:34 -0500

    feat: :sparkles: Creacion de los Dtos unicos de cada entidad

---
+++ API/Dtos/TeamDriverDto.cs
@@ -0,0 +1,9 @@
+namespace API.Dtos;
+
+public class TeamDriverDto
+{
+    public int IdTeam { get; set; }
+    //public ICollection<TeamDto> teams { get; set; }
+    public int IdDriver { get; set; }
+    //public ICollection<DriverDto> drivers { get; set; }
+}
---
+++ API/Dtos/TeamDto.cs
@@ -0,0 +1,7 @@
+namespace API.Dtos;
+
+public class TeamDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
